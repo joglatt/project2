663f5a493043ef4bc204ca84c3838f337fade46a
olivier-dev <omartineau8@gmail.com>
2018-05-24 20:12:59 -0400

    initial dependencies plus seeds and schema files

---
+++ DB/seeds.sql
@@ -0,0 +1,11 @@
+insert into UserInfo (name, sex, age, weight, height)
+values("Olivier", true, 27, 230, 71),
+("Josh",true, 28, 175, 74),
+("Pat", true, 35, 160, 68);
+
+insert into Workouts (UID, type, duration, calories)
+values(1, "cardio", 75, 300),
+(1, "resistance", 30, 300),
+(1, "cardio", 60, 280),
+(2, "resistance", 70, 400),
+(3, "cardio", 50, 400);

15e9bfa0d74b54e5ab26d493efc93973284885f5
joglatt <34425753+joglatt@users.noreply.github.com>
2018-05-31 21:00:39 -0400

    user id

--- public/js/members.js
+++ public/js/members.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
+    //data.id is the user id being passed from the api
+    console.log(data.id);
   });
 });
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,18 +2,16 @@
 var path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = function (req, res, next){
-  
+var isAuthenticated = function(req, res, next) {
   if (req.user) {
     return next();
   }
 
   // If the user isn't logged in, redirect them to the login page
   return res.redirect("/");
-}
+  };
 
 module.exports = function(app) {
-
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
@@ -25,15 +23,15 @@ module.exports = function(app) {
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/members/");
+      console.log(req.user);
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
+  app.get("/members/", isAuthenticated, function(req, res) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
-
 };

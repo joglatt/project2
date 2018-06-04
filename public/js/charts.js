3a1ae6b03b48226886cf59bef1442d0bf6b83e3e
Michael Myers <michaelmyers@Michaels-MBP.home>
2018-06-04 19:39:59 -0400

    highchart

---
+++ public/js/charts.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    // I've got this chart reading as a workout duration chart but we can modify 
+    // based on which charts we are going to build. 
+    $(function () {
+        var $container = $('#chart1').appendTo('#workoutDurationChart');
+
+        window.chart = new Highcharts.Chart({
+            chart: {
+                renderTo: $container[0],
+                height: 400
+            },
+
+            xAxis: {
+                categories: ['Sun', 'Mon', 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat'],
+                title: {
+                    text: 'Weekdays'
+                }
+            },
+
+            yAxis: {
+                title: {
+                    text: 'Workout Length (mins)'
+                }
+            },
+
+            title: {
+                text: 'Workout Durations'
+            },
+
+            plotOptions: {
+                series: {
+                    allowPointSelect: true
+                }
+            },
+
+            series: [{
+                showInLegend: false,
+                // series.data is what we will need to fill with stats for the chart
+                data: []
+            }]
+        });
+    });
+
+});

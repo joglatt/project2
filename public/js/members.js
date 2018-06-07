0f99b1e5741813ef3177e24372f7b530ae5d4d2d
olivier-dev <omartineau8@gmail.com>
2018-06-07 16:31:03 -0400

    working tables

--- public/js/members.js
+++ public/js/members.js
@@ -52,32 +52,23 @@ $(document).ready(function() {
       type: "GET",
       data: userId,
       success: function (data) {
-          console.log(data);   
+          // console.log(data);   
       },
     }).then(function(data) {
-      // console.log(data)
       for (var i = 0; i < data.length; i++) {
-        console.log(data[i].WID + " | " + data[i].UID + " | " + data[i].type + " | " + data[i].duration + " | " + data[i].calories);
-    }
-    })
-      
+        // console.log(data[i].WID + " | " + data[i].UID + " | " + data[i].type + " | " + data[i].duration + " | " + data[i].calories);
+          var workoutID = data[i].WID;
+          var workoutUserId = data[i].UID;
+          var workoutType = data[i].type;
+          var workoutDuration = data[i].duration;
+          var workoutCalories = data[i].calories;
+          console.log(workoutID, workoutUserId, workoutType, workoutDuration, workoutCalories)
+          var tableContent = "<tr><td>" + workoutID + "</td><br><td>" + workoutUserId + "</td><td>" + workoutType + "</td><td>" + workoutDuration + "</td><td>" + workoutCalories + "</td></tr>";
+        };
+        $("#workoutTable").html("<tr><th>Workout ID</th><br><th>User ID</th><br><th>Type</th><br><th>Duration</th><br><th>Calories</th><tr>" + tableContent);
+        $("workoutTable").append(tableContent);
     
+    });
   }
 });
 
-// $.ajax("/api/user_data/", {
-//   type: "PUT",
-//   data: newWeight
-// }).then(
-//   function() {
-//     console.log("changed weight to", newWeight.weight);
-//     // Reload the page to get the updated list
-//     location.reload();
-//   }
-// );
-
-// $.put("/api/user_data", newWeight, function(data) {
-//   if (data) {
-//     alert("Weight Updated");
-//   }
-// });
